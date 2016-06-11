4dce80463df05aa033f0907c54e80c1e991c35fa
Grant Dennison <grant.dennison@gmail.com>
2016-06-11 18:04:52 -0500

    Named new sprite abstraction Sprite

--- src/static/sprite_new.js
+++ src/static/sprite_new.js
@@ -1,4 +1,4 @@
-SplitTime.NewSprte = function(src, xres, yres, offX, offY) {
+SplitTime.Sprite = function(src, xres, yres, offX, offY) {
     this.src = src;
     this.xres = xres;
     this.yres = yres;
@@ -9,20 +9,20 @@ SplitTime.NewSprte = function(src, xres, yres, offX, offY) {
     }
 };
 
-// SplitTime.NewSprte.prototype.src;
-SplitTime.NewSprte.prototype.dir = "S";
-SplitTime.NewSprte.prototype.xres = 32;
-SplitTime.NewSprte.prototype.yres = 64;
-SplitTime.NewSprte.prototype.offX = 0;
-SplitTime.NewSprte.prototype.offY = 0;
-SplitTime.NewSprte.prototype.sx = 0;
-SplitTime.NewSprte.prototype.sy = 0;
-SplitTime.NewSprte.prototype.stance = "default";
-SplitTime.NewSprte.prototype.requestedStance = "default";
-SplitTime.NewSprte.prototype.requestedFrameReset = false;
-SplitTime.NewSprte.prototype.frame = 0;
+// SplitTime.Sprite.prototype.src;
+SplitTime.Sprite.prototype.dir = "S";
+SplitTime.Sprite.prototype.xres = 32;
+SplitTime.Sprite.prototype.yres = 64;
+SplitTime.Sprite.prototype.offX = 0;
+SplitTime.Sprite.prototype.offY = 0;
+SplitTime.Sprite.prototype.sx = 0;
+SplitTime.Sprite.prototype.sy = 0;
+SplitTime.Sprite.prototype.stance = "default";
+SplitTime.Sprite.prototype.requestedStance = "default";
+SplitTime.Sprite.prototype.requestedFrameReset = false;
+SplitTime.Sprite.prototype.frame = 0;
 
-SplitTime.NewSprte.prototype.stances = {
+SplitTime.Sprite.prototype.stances = {
     "default": {
         "S": 0,
         "N": 1,
@@ -32,7 +32,7 @@ SplitTime.NewSprte.prototype.stances = {
 };
 
 
-SplitTime.NewSprte.prototype.draw = function(ctx) {
+SplitTime.Sprite.prototype.draw = function(ctx) {
     if(!ctx)
     {
         ctx = SplitTime.see;
@@ -46,7 +46,7 @@ SplitTime.NewSprte.prototype.draw = function(ctx) {
     ctx.drawImage(tImg, this.sx, this.sy, this.xres, this.yres, x, y, this.xres, this.yres);
 };
 
-SplitTime.NewSprte.prototype.finalizeFrame = function() {
+SplitTime.Sprite.prototype.finalizeFrame = function() {
     if(this.stance != this.requestedStance || this.requestedFrameReset) {
         this.frame = 0;
     }
@@ -63,7 +63,7 @@ SplitTime.NewSprte.prototype.finalizeFrame = function() {
     }
 };
 
-SplitTime.NewSprte.prototype.finalizeStance = function() {
+SplitTime.Sprite.prototype.finalizeStance = function() {
     //Allow for non-complicated spritesheets with one column
     if(!this.stances) {
         this.column = 0;
@@ -99,15 +99,15 @@ SplitTime.NewSprte.prototype.finalizeStance = function() {
     this.sy = this.yres*this.frame;
 };
 
-SplitTime.NewSprte.prototype.getImage = function() {
+SplitTime.Sprite.prototype.getImage = function() {
     return SplitTime.getImage(this.src);
 };
 
-SplitTime.NewSprte.prototype.requestStance = function(stance, forceReset) {
+SplitTime.Sprite.prototype.requestStance = function(stance, forceReset) {
     this.requestedStance = stance;
     this.requestedFrameReset = forceReset;
 };
 
-SplitTime.NewSprte.prototype.setDirection = function(dir) {
+SplitTime.Sprite.prototype.setDirection = function(dir) {
     this.dir = dir;
 };
