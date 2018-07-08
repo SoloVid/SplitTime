e8f335ac779e1a0447e465cdbf72162e8017e6ef
Grant Dennison <grant.dennison@gmail.com>
2018-07-07 22:34:39 -0400

    Got sticky corners working (UX) better than ever!

--- src/engine/body/zeldaMove.js
+++ src/engine/body/zeldaMove.js
@@ -74,7 +74,7 @@ SplitTime.Body.prototype.zeldaCheckStepBodies = function() {
 						//The .pushing here ensures that there is no infinite loop of pushing back and forth
 						if(this.pushy && body.pushy && this.pushedBodies.indexOf(body) < 0) {
 							this.pushing = true; //prevent counter-push
-							var moved = body.zeldaBump(this._totalStepDistance/2, this.dir);
+							var moved = body.zeldaBump(this._totalStepDistance/2, dirToOther);
 							this.pushing = false;
 
 							if(moved) {
@@ -131,61 +131,102 @@ SplitTime.Body.prototype.zeldaStep = function(distance) {
 
 	// TODO: intertwine x and y pixel steps
 
-	//Handle y movement
-	var outY = false;
-	var stoppedY = false;
+	var maxIterations = adx + ady;
+	var xPixelsRemaining = adx;
+	var yPixelsRemaining = ady;
+
+    var outY = false;
+    var stoppedY = false;
     var pixelsMovedY = 0;
-    for(var j = 0; j < ady; j++) {
-		this.y += jhat;
-		//Check if out of bounds
-		if(this.y >= level.height || this.y < 0) {
-            outY = true;
-		} else {
-            stoppedY = !this.zeldaCheckStep("y", "x", dy > 0);
-		}
 
-        // this.stepDistanceRemaining--;
+    var outX = false;
+    var stoppedX = false;
+    var pixelsMovedX = 0;
 
-		if(stoppedY || outY) {
-			this.y -= jhat;
-			break;
-		} else {
-			pixelsMovedY++;
-		}
-	}
+    for(var i = 0; i < maxIterations; i++) {
+        if(xPixelsRemaining > 0) {
+            this.x += ihat;
+            if(this.x >= level.width || this.x < 0) {
+                outX = true;
+            }
+            else {
+                stoppedX = !this.zeldaCheckStep("x", "y", dx > 0);
+            }
+
+            // this.stepDistanceRemaining--;
+
+            if(stoppedX || outX) {
+                this.x -= ihat;
+            } else {
+                xPixelsRemaining--;
+                pixelsMovedX++;
+            }
+        }
+
+        if(yPixelsRemaining > 0) {
+            this.y += jhat;
+            //Check if out of bounds
+            if(this.y >= level.height || this.y < 0) {
+                outY = true;
+            }
+            else {
+                stoppedY = !this.zeldaCheckStep("y", "x", dy > 0);
+            }
+
+            if(stoppedY || outY) {
+                this.y -= jhat;
+            } else {
+                yPixelsRemaining--;
+                pixelsMovedY++;
+            }
+        }
+    }
+
+	//Handle y movement
+    // for(var j = 0; j < ady; j++) {
+	// 	this.y += jhat;
+	// 	//Check if out of bounds
+	// 	if(this.y >= level.height || this.y < 0) {
+     //        outY = true;
+	// 	} else {
+     //        stoppedY = !this.zeldaCheckStep("y", "x", dy > 0);
+	// 	}
+    //
+     //    // this.stepDistanceRemaining--;
+    //
+	// 	if(stoppedY || outY) {
+	// 		this.y -= jhat;
+	// 		break;
+	// 	} else {
+	// 		pixelsMovedY++;
+	// 	}
+	// }
 	if(ady > 0 && !stoppedY && !outY) {
 		// Subtract off any overshoot
 		this.y -= (dyRounded - dy);
-    } else if(pixelsMovedY === 1) {
-        this.y -= 0.9*jhat;
     }
 
 	//Handle x movement;
-	var outX = false;
-	var stoppedX = false;
-	var pixelsMovedX = 0;
-	for(var i = 0; i < adx; i++) {
-		this.x += ihat;
-		if(this.x >= level.width || this.x < 0) {
-			outX = true;
-		} else {
-			stoppedX = !this.zeldaCheckStep("x", "y", dx > 0);
-		}
-
-        // this.stepDistanceRemaining--;
-
-        if(stoppedX || outX) {
-			this.x -= ihat;
-			break;
-		} else {
-        	pixelsMovedX++;
-		}
-	}
+	// for(var i = 0; i < adx; i++) {
+	// 	this.x += ihat;
+	// 	if(this.x >= level.width || this.x < 0) {
+	// 		outX = true;
+	// 	} else {
+	// 		stoppedX = !this.zeldaCheckStep("x", "y", dx > 0);
+	// 	}
+    //
+     //    // this.stepDistanceRemaining--;
+    //
+     //    if(stoppedX || outX) {
+	// 		this.x -= ihat;
+	// 		break;
+	// 	} else {
+     //    	pixelsMovedX++;
+	// 	}
+	// }
     if(adx > 0 && !stoppedX && !outX) {
         // Subtract off any overshoot
         this.x -= (dxRounded - dx);
-    } else if(pixelsMovedX === 1) {
-		this.x -= 0.9*ihat;
 	}
 
     this.nearbyBodies = null;
@@ -195,7 +236,7 @@ SplitTime.Body.prototype.zeldaStep = function(distance) {
     //If stopped, help person out by sliding around corner
     var stopped = stoppedX || stoppedY;
     var out = outX || outY;
-	if(stopped && !out) {
+	if(stopped && !out && pixelsMovedX + pixelsMovedY < distance / 2) {
 		this.zeldaSlide(distance / 2);
 	}
 
@@ -211,45 +252,146 @@ SplitTime.Body.prototype.zeldaSlide = function(maxDistance) {
 
     var level = this.getLevel();
     var halfBase = Math.round(this.baseLength/2);
+
+    //Note: The commented diagonal algorithm below seems to not behave as desirably as the simpler method
     var x = Math.round(this.x);
     var y = Math.round(this.y);
     var z = Math.round(this.z);
 
     var dist = maxDistance; //Math.min(1, maxDistance);
 
-    // for(var i = 0; i < 1; i++) {
-
 	// Closest diagonal direction positive angle from current direction
     var positiveDiagonal = (Math.round(this.dir + 1.1) - 0.5) % 4;
     // Closest diagonal direction negative angle from current direction
     var negativeDiagonal = (Math.round(this.dir + 3.9) - 0.5) % 4;
 
-    var iPositiveCorner = SplitTime.pixCoordToIndex(
-        x + SplitTime.Direction.getXSign(positiveDiagonal) * (halfBase + 1),
-        y + SplitTime.Direction.getYSign(positiveDiagonal) * (halfBase + 1),
-        level.layerFuncData[z]
-    );
-    var iNegativeCorner = SplitTime.pixCoordToIndex(
-        x + SplitTime.Direction.getXSign(negativeDiagonal) * (halfBase + 1),
-        y + SplitTime.Direction.getYSign(negativeDiagonal) * (halfBase + 1),
-        level.layerFuncData[z]
-    );
+    function isCornerOpen(direction, howFarAway) {
+        var iCorner = SplitTime.pixCoordToIndex(
+            x + SplitTime.Direction.getXSign(direction) * (halfBase + howFarAway),
+            y + SplitTime.Direction.getYSign(direction) * (halfBase + howFarAway),
+            level.layerFuncData[z]
+        );
 
-    var isPositiveCornerOpen = level.layerFuncData[z].data[iPositiveCorner] !== 255;
-    var isNegativeCornerOpen = level.layerFuncData[z].data[iNegativeCorner] !== 255;
+        return level.layerFuncData[z].data[iCorner] !== 255;
+    }
 
-    // if(SplitTime.FrameStabilizer.haveSoManyMsPassed(200)) {
-    //     console.log("positive: " + positiveDiagonal + " (" + isPositiveCornerOpen + "), negative: " + negativeDiagonal + " (" + isNegativeCornerOpen + ")");
+    var MAX_RECURSION = 5;
+    var me = this;
+    function bumpMoreDesirableWay(dir1, dir2, howFarOut) {
+        var isCorner1Open = isCornerOpen(dir1, howFarOut);
+        var isCorner2Open = isCornerOpen(dir2, howFarOut);
+        if(isCorner1Open && isCorner2Open) {
+            if(howFarOut > MAX_RECURSION) {
+                // Tie; do nothing
+            } else {
+                bumpMoreDesirableWay(dir1, dir2, howFarOut + 1);
+            }
+        } else if(isCorner1Open) {
+            me.zeldaBump(dist, dir1);
+        } else if(isCorner2Open) {
+            me.zeldaBump(dist, dir2);
+        }
+    }
+
+    bumpMoreDesirableWay(positiveDiagonal, negativeDiagonal, 1);
+
+    // Note: Below is the same as the above method without recursion
+    // var iPositiveCorner = SplitTime.pixCoordToIndex(
+    //     x + SplitTime.Direction.getXSign(positiveDiagonal) * (halfBase + 1),
+    //     y + SplitTime.Direction.getYSign(positiveDiagonal) * (halfBase + 1),
+    //     level.layerFuncData[z]
+    // );
+    // var iNegativeCorner = SplitTime.pixCoordToIndex(
+    //     x + SplitTime.Direction.getXSign(negativeDiagonal) * (halfBase + 1),
+    //     y + SplitTime.Direction.getYSign(negativeDiagonal) * (halfBase + 1),
+    //     level.layerFuncData[z]
+    // );
+
+    // var isPositiveCornerOpen = level.layerFuncData[z].data[iPositiveCorner] !== 255;
+    // var isNegativeCornerOpen = level.layerFuncData[z].data[iNegativeCorner] !== 255;
+
+    // if(isPositiveCornerOpen && isNegativeCornerOpen) {
+     //        // Tie; do nothing
+	// } else if(isPositiveCornerOpen) {
+    	// this.zeldaBump(dist, positiveDiagonal);
+	// } else if(isNegativeCornerOpen) {
+    	// this.zeldaBump(dist, negativeDiagonal);
+	// }
+
+	// Note: Below is the simple method
+    // var dir = SplitTime.Direction.simplifyToCardinal(this.dir);
+    // var x = Math.round(this.x);
+    // var y = Math.round(this.y);
+    // var z = Math.round(this.z);
+    //
+    // // var dist = 0.1;
+    // var dist = maxDistance;
+    // // var dist = Math.min(1, maxDistance);
+    //
+    // // for(var i = 0; i < 1; i++) {
+    // // var iNE, iNW, iSW, iSE;
+    // // var isNEOpen, isNWOpen, isSWOpen, isSEOpen;
+    // var iNE = SplitTime.pixCoordToIndex(x + halfBase + 1, y - halfBase - 1, level.layerFuncData[z]);
+    // var iNW = SplitTime.pixCoordToIndex(x - halfBase - 1, y - halfBase - 1, level.layerFuncData[z]);
+    // var iSW = SplitTime.pixCoordToIndex(x - halfBase - 1, y + halfBase + 1, level.layerFuncData[z]);
+    // var iSE = SplitTime.pixCoordToIndex(x + halfBase + 1, y + halfBase + 1, level.layerFuncData[z]);
+    // var isNEOpen = level.layerFuncData[z].data[iNE] !== 255;
+    // var isNWOpen = level.layerFuncData[z].data[iNW] !== 255;
+    // var isSWOpen = level.layerFuncData[z].data[iSW] !== 255;
+    // var isSEOpen = level.layerFuncData[z].data[iSE] !== 255;
+    // if(dir === SplitTime.Direction.E) {
+    //     // iNE = SplitTime.pixCoordToIndex(x + halfBase, y - halfBase - 1, level.layerFuncData[z]);
+    //     // iSE = SplitTime.pixCoordToIndex(x + halfBase, y + halfBase + 1, level.layerFuncData[z]);
+    //     // isNEOpen = level.layerFuncData[z].data[iNE] !== 255;
+    //     // isSEOpen = level.layerFuncData[z].data[iSE] !== 255;
+    //     if(isNEOpen && isSEOpen) {
+    //     	console.log("tie");
+    //         // Do nothing; tie
+    //     } else if(isNEOpen && SplitTime.Direction.areWithin90Degrees(this.dir, SplitTime.Direction.N, 1.1)) {
+    //         this.zeldaBump(dist, SplitTime.Direction.N);
+    //     } else if(isSEOpen && SplitTime.Direction.areWithin90Degrees(this.dir, SplitTime.Direction.S, 1.1)) {
+    //         this.zeldaBump(dist, SplitTime.Direction.S);
+    //     }
+    // } else if(dir === SplitTime.Direction.N) {
+    //     // iNE = SplitTime.pixCoordToIndex(x + halfBase + 1, y - halfBase, level.layerFuncData[z]);
+    //     // iNW = SplitTime.pixCoordToIndex(x - halfBase - 1, y - halfBase, level.layerFuncData[z]);
+    //     // isNEOpen = level.layerFuncData[z].data[iNE] !== 255;
+    //     // isNWOpen = level.layerFuncData[z].data[iNW] !== 255;
+    //     if(isNEOpen && isNWOpen) {
+    //         console.log("tie");
+    //         // Do nothing; tie
+    //     } else if(isNEOpen && SplitTime.Direction.areWithin90Degrees(this.dir, SplitTime.Direction.E, 1.1)) {
+    //         this.zeldaBump(dist, SplitTime.Direction.E);
+    //     } else if(isNWOpen && SplitTime.Direction.areWithin90Degrees(this.dir, SplitTime.Direction.W, 1.1)) {
+    //         this.zeldaBump(dist, SplitTime.Direction.W);
+    //     }
+    // } else if(dir === SplitTime.Direction.W) {
+    //     // iNW = SplitTime.pixCoordToIndex(x - halfBase, y - halfBase - 1, level.layerFuncData[z]);
+    //     // iSW = SplitTime.pixCoordToIndex(x - halfBase, y + halfBase + 1, level.layerFuncData[z]);
+    //     // isNWOpen = level.layerFuncData[z].data[iNW] !== 255;
+    //     // isSWOpen = level.layerFuncData[z].data[iSW] !== 255;
+    //     if(isNWOpen && isSWOpen) {
+    //         console.log("tie");
+    //         // Do nothing; tie
+    //     } else if(isNWOpen && SplitTime.Direction.areWithin90Degrees(this.dir, SplitTime.Direction.N, 1.1)) {
+    //         this.zeldaBump(dist, SplitTime.Direction.N);
+    //     } else if(isSWOpen && SplitTime.Direction.areWithin90Degrees(this.dir, SplitTime.Direction.S, 1.1)) {
+    //         this.zeldaBump(dist, SplitTime.Direction.S);
+    //     }
+    // } else if(dir === SplitTime.Direction.S) {
+    //     // iSW = SplitTime.pixCoordToIndex(x - halfBase - 1, y + halfBase, level.layerFuncData[z]);
+    //     // iSE = SplitTime.pixCoordToIndex(x + halfBase + 1, y + halfBase, level.layerFuncData[z]);
+    //     // isSWOpen = level.layerFuncData[z].data[iSW] !== 255;
+    //     // isSEOpen = level.layerFuncData[z].data[iSE] !== 255;
+    //     if(isSEOpen && isSWOpen) {
+    //         console.log("tie");
+    //         // Do nothing; tie
+    //     } else if(isSEOpen && SplitTime.Direction.areWithin90Degrees(this.dir, SplitTime.Direction.E, 1.1)) {
+    //         this.zeldaBump(dist, SplitTime.Direction.E);
+    //     } else if(isSWOpen && SplitTime.Direction.areWithin90Degrees(this.dir, SplitTime.Direction.W, 1.1)) {
+    //         this.zeldaBump(dist, SplitTime.Direction.W);
+    //     }
     // }
 
-    if(isPositiveCornerOpen && isNegativeCornerOpen) {
-    	// Tie; do nothing
-	} else if(isPositiveCornerOpen) {
-    	this.zeldaBump(dist, positiveDiagonal);
-	} else if(isNegativeCornerOpen) {
-    	this.zeldaBump(dist, negativeDiagonal);
-	}
-    // }
-
-	this._sliding = false;
+    this._sliding = false;
 };
