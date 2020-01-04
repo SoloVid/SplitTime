9df13f5857c8b6267dfe09ab1239ab06b2aa762d
Grant Dennison <grant.dennison@gmail.com>
2020-01-04 16:45:18 -0500

    Documenting audio loading considerations
    # Conflicts:
    #	src/engine/AudioManager.js

--- src/engine/AudioManager.ts
+++ src/engine/AudioManager.ts
@@ -1,3 +1,13 @@
+/**
+ * Audio loading considerations
+ * 
+ * The current setup is that each audio file is loaded only once, when the play function
+ * is called on it for the first time. Then the Howl object associated with that audio
+ * file gets added to loadedSounds[handle] to be used later.
+ * 
+ * Note: depending on the total number of audio files used in a project, it may be better to 
+ * load all the files at the start of the program.
+ */
 namespace SplitTime.audio {
     var AUDIO_ROOT: string;
     defer(() => {
