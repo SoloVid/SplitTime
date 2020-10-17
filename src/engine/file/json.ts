2778ef6239031060b7df69003f1c0743b3843b92
Grant Dennison <grant.dennison@gmail.com>
2020-10-16 23:18:43 -0400

    WIP Broken first pass at fixing jsonable type stuff

--- src/engine/file/json.ts
+++ src/engine/file/json.ts
@@ -1,13 +1,129 @@
 namespace splitTime.file {
-    export type jsonable =
+    // From https://medium.com/dailyjs/typescript-create-a-condition-based-subset-types-9d902cea5b8c
+    // type FilterFlags<Base, Condition> = {
+    //     [Key in keyof Base]: 
+    //         Base[Key] extends Condition ? Key : never
+    // };
+    // type AllowedNames<Base, Condition> = 
+    //         FilterFlags<Base, Condition>[keyof Base];
+    // type SubType<Base, Condition> = 
+    //         Pick<Base, AllowedNames<Base, Condition>>;
+
+            
+    type primitive = null
+        | boolean
+        | number
+        | string
+    // export type jsonable = primitive
+    //     | jsonable[]
+    //     | { [key: string]: jsonable }
+    //     | { [key: number]: jsonable }
+
+    type jsonable =
+        | null
         | boolean
         | number
         | string
         | jsonable[]
-        | { [key: string]: jsonable }
-        | { [key: number]: jsonable }
-    export type MadeJsonable<T> = { [K in keyof T]: T[K] }
+        | { [prop: string]: jsonable };
+    // type jsonable = primitive | jsonable_object | jsonable_array
+
+    // interface jsonable_object {
+    //     // [x: string]: string | number | boolean | jsonable_object | jsonable_array;
+    //     [x: string]: jsonable;
+    // }
+
+    // // interface jsonable_object2 {
+    // //     [x: number]: jsonable
+    // // }
+
+    // interface jsonable_array extends Array<jsonable> { }
+
+
+    // type MadeJsonable<T> = T extends (infer U)[] ? MadeJsonable<U>[] : SubType<T, jsonable>
+    // export type IsJsonable<T> = 
+    //     T extends primitive
+    //         ? T
+    //         : T extends (infer U)[]
+    //             ? IsJsonable<U> extends U
+    //                 ? T
+    //                 : never
+    //             : T extends MadeJsonable<T>
+    //                 ? MadeJsonable<T> extends T
+    //                     ? T
+    //                     : never
+    //                 : never
+    // export type MadeJsonable<T> = 
+    //     { [K in keyof T]: T[K] extends jsonable
+    //         ? T[K]
+    //         : T[K] extends Function
+    //         ? never
+    //         : MadeJsonable<T[K]> extends jsonable
+    //         ? MadeJsonable<T[K]>
+    //         : never
+    //     }
+
+    type DefinitelyNotJsonable = (() => any) | undefined
+
+    // From https://github.com/microsoft/TypeScript/issues/1897#issuecomment-580962081
+    export type IsJsonable<T> = T extends DefinitelyNotJsonable
+        ? never
+        : T extends primitive
+            // Primitive is acceptable
+            ? T
+            : T extends (infer U)[]
+                // Arrays are special; just check array element type
+                ? IsJsonable<U>[]
+                : T extends object
+                    ? {
+                        // Iterate over keys in object case
+                        // [P in keyof T]: IsJsonable<T[P]>
+                        [P in keyof T]: T[P] extends jsonable
+                            ? T[P]
+                            // I don't understand this part, and it didn't seem to work for me
+                            // : Pick<T, P> extends Required<Pick<T, P>>
+                            // ? never
+                            : T[P] extends DefinitelyNotJsonable
+                                // Don't allow function or undefined
+                                ? never
+                                : IsJsonable<T[P]>
+                    }
+                    // any other non-object no bueno
+                    : never
+
+    type FD = level.FileData
+    type FDP = FD extends primitive ? "yes" : "no"
+    type FDA = FD extends (infer U)[] ? "yes" : "no"
+    type FDMJ = { [P in keyof FD]: FD[P] extends jsonable ? "yes" : "no" }
+    type FDMP = { [P in keyof FD]: Pick<FD, P> extends Required<Pick<FD, P>> ? "yes" : "no" }
+    type P = Pick<FD, "layers">
+    type R = Required<Pick<FD, "layers">>
+    type A = level.file_data.Layer[]
+
     export type json = string
+
+    // let as = [1, "himom", {}]
+    // type a1 = typeof as
+    // type a2 = MadeJsonable<a1>
+    // let as2: file.IsJsonable<typeof as> = as
+
+    // type b = level.FileData
+    // type bt = JsonCompatible<b>
+    // type b2 = MadeJsonable<b>
+    // type b3 = SubType<b, jsonable>
+
+    type la = level.file_data.Layer[]
+    type lat = la extends (infer U)[] ? "yes" : "no"
+    // type lat = la extends { [key: string]: string | number }[] ? "yes" : never
+    // type lak = keyof la
+    // type la2 = MadeJsonable<la>
+    // type la2k = keyof la2
+
+    // type l = level.file_data.Layer
+    // type l2 = MadeJsonable<l>
+
+    // type ll = l extends jsonable ? "yes" : "no"
+
 
     export function toJsonable(thing: unknown): jsonable {
         return JSON.parse(JSON.stringify(thing))
