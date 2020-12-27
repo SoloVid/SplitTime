7e151309af59919f14826e19dab2fd1747aafaf5
Grant Dennison <grant.dennison@gmail.com>
2020-12-27 02:24:21 -0500

    Add documentation for PropPostProcessor

--- src/engine/world/prop-post-processor.ts
+++ src/engine/world/prop-post-processor.ts
@@ -3,13 +3,29 @@ namespace splitTime.world {
     export class PropPostProcessor {
         private readonly processorMap: { [id: string]: Processor } = {}
 
+        /**
+         * Register a callback to be run for each prop created from a montage with specified
+         * propPostProcessor immediately after the prop is created and placed into the level.
+         *
+         * The purpose of this functionality is to allow injection of non-standard behavior
+         * to prop objects, since there is otherwise no good way to use level props as anything
+         * more than simple bodies.
+         *
+         * Note that, by the time the callback is called, the prop body will already have
+         * everything set as it would if it had no post processor, including its location in the level.
+         *
+         * @param processorId "propPostProcessor" as specified in collage editor
+         * @param processor callback to process prop after creation
+         */
         register(processorId: string, processor: Processor): void {
-            if (DEBUG) {
-                assert(!(processorId in this.processorMap), "Processor \"" + processorId + "\" already registered")
-            }
+            assert(!(processorId in this.processorMap), "Processor \"" + processorId + "\" already registered")
             this.processorMap[processorId] = processor
         }
 
+        /**
+         * Actually run a post processor for a newly instantiated prop.
+         * This method is for engine use only and should not be called by game code.
+         */
         process(processorId: string, newProp: SpriteBody, rawProp: level.file_data.Prop): void {
             if (DEBUG) {
                 assert(processorId in this.processorMap, "Processor \"" + processorId + "\" not registered")
