317bfb74ffadfad682f14738d5ca67d3e3510fe5
Grant Dennison <grant.dennison@gmail.com>
2023-04-01 16:45:27 -0400

    Only tick editor time when window is active

--- src/editor/client/time-context.tsx
+++ src/editor/client/time-context.tsx
@@ -5,16 +5,30 @@ export const Time = createContext<number>(0)
 
 export function TimeProvider({ children }: { children: any }) {
   const [time, setTime] = useState(0)
+  const [mouseInDoc, setMouseInDoc] = useState(false)
+
+  useEffect(() => {
+    const over = () => setMouseInDoc(true)
+    const out = () => setMouseInDoc(false)
+    window.addEventListener("mouseover", over)
+    window.addEventListener("mouseout", out)
+    return () => {
+      window.removeEventListener("mouseover", over)
+      window.removeEventListener("mouseout", out)
+    }
+  }, [setMouseInDoc])
   
   useEffect(() => {
     const TIME_INTERVAL = 50;
     const handle = setInterval(function() {
-      setTime((oldTime) => oldTime += TIME_INTERVAL / 1000)
+      if (document.visibilityState === "visible" && (document.hasFocus() || mouseInDoc)) {
+        setTime((oldTime) => oldTime += TIME_INTERVAL / 1000)
+      }
     }, TIME_INTERVAL)
     return () => {
       clearInterval(handle)
     }
-  }, [])
+  }, [mouseInDoc])
 
   return (
     <Time.Provider value={time}>
